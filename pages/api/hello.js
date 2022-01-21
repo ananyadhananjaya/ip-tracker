64d1cc83d9f3bb5dddf6649c46cb5d993ab85406
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-01-21 11:21:04 +0530

    ip-tracker basic app completed

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    fetch("http://ip-api.com/json/223.226.21.15")
+    .then((res)=>{
+      console.log(res.json())
+    })
+    .catch((e)=>{
+      console.log(e)
+    })
+  
 }
